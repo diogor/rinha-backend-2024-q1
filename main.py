988d2836f6800fc7ca994d81d8ce598b76231832
Diogo Lima <diogo@dr6.com.br>
2024-02-26 23:44:26 -0300

    connection pool

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 from dataclasses import dataclass
 from typing import Literal
-from blacksheep import Application, FromJSON, post
+from blacksheep import Application, FromJSON, Response, post
 
-from services import create_transaction
+from services import NotFoundError, create_transaction
 
 app = Application()
 
@@ -16,8 +16,11 @@ class TransactionRequest:
 @post("/clientes/{client_id}/transacoes")
 async def index(client_id: int, input: FromJSON[TransactionRequest]):
     transaction = input.value
-    create_transaction(client_id, transaction.valor, transaction.descricao, transaction.tipo)
+    try:
+        await create_transaction(client_id, transaction.valor, transaction.descricao, transaction.tipo)
+    except NotFoundError:
+        return Response(status=404)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT")))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 3000)))
--- services.py
+++ services.py
@@ -1,14 +1,36 @@
 import os
 from datetime import datetime
-from typing import Literal
-import psycopg2
+from typing import Literal, Tuple
+import psycopg
+from psycopg_pool import ConnectionPool
 
-conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+conn = ConnectionPool(os.getenv("DATABASE_URL"))
 
-def create_transaction(client_id: int, amount: int, descricao: str, tipo: Literal["c", "d"]) -> None:
-    cur = conn.cursor()
-    cur.execute(
-        'INSERT INTO transacoes (codigo_cliente, valor, descricao, tipo, data_transacao) VALUES (%s, %s, %s, %s, %s)',
-        (client_id, amount, descricao, tipo, datetime.now())
-    )
-    conn.commit()
+class NotFoundError(Exception):
+    pass
+
+async def create_transaction(client_id: int, amount: int, descricao: str, tipo: Literal["c", "d"]) -> None:
+    try:
+        with conn.connection() as c:
+            with c.cursor() as cur:
+                cur.execute(
+                    'INSERT INTO transacoes (codigo_cliente, valor, descricao, tipo, data_transacao) VALUES (%s, %s, %s, %s, %s)',
+                    (client_id, amount, descricao, tipo, datetime.now())
+                )
+    except psycopg.errors.ForeignKeyViolation:
+        raise NotFoundError("Client not found")
+
+
+def get_balance(client_id: int) -> Tuple[int, int]:
+    with conn.getconn() as c:
+        with c.cursor() as cur:
+            cur.execute(
+                "SELECT saldo, limite FROM clientes WHERE id = %s",
+                (client_id,)
+            )
+            res = cur.fetchone()
+
+    if not res:
+        raise NotFoundError("Client not found")
+
+    return res[0], res[1]
